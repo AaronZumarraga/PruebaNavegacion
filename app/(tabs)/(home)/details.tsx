0e72a727aa9ea35c78fa4ebc93ad6476164a6b8d
Aaron Zumarraga <aaron.zumarraga@hotmail.com>
2025-04-24 12:47:03 -0500

    ingreso de nombre

--- app/(tabs)/(home)/details.tsx
+++ app/(tabs)/(home)/details.tsx
@@ -1,9 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function DetailsScreen() {
+  const [name, setName] = useState('');
+
   return (
     <View style={styles.container}>
       <Text>Detalles de la pagina de inicio</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ingresa tu nombre"
+        value={name}
+        onChangeText={setName}
+      />
     </View>
   );
 }
@@ -13,5 +22,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 20,
+    paddingHorizontal: 10,
+    width: '80%',
   },
 });
